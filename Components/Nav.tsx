8bcd9bbad7de5f5cf93a85fbb16104fed916b8da
unknown <brightstarmoxiz2@gmail.com>
2024-12-18 18:12:54 +0100

    MOBILE-NAVBAR

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -1,17 +1,29 @@
 import { Bars3CenterLeftIcon } from '@heroicons/react/20/solid';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface Props {
     openNav :()=>void
 }
 
 const Nav = ({openNav}:Props) => {
+
+const[navSticky, setNavSticky] = useState(false)
+
+useEffect(()=>{
+const handler =()=>{
+    if(window.scrollY>=90){
+        setNavSticky(true)
+    }
+    if(window)
+}
+},[])
+
   return (
     <div className='fixed w-[100%] z-[1000] bg-gray-800'>
      <div className='flex items-center justify-between w-[80%] mx-auto h-[12vh]'>
-       <div className='font-mono text-white text-[18px]'>
-        <span className='text-[30px] md:text-yellow-400'>MR</span>
-        Coder
+       <div className='font-serif font-extrabold text-white text-[22px]'>
+        <span className='text-[30px] text-yellow-400'>Big</span>
+       Tech
        </div>
        <ul className='md:flex hidden items-center space-x-10'>
         <li className='nav_link'><a  href='#'></a>Home</li>
