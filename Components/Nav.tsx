8e9e89229c83ad0fcf60c11271ae0e89ed8a7435
unknown <brightstarmoxiz2@gmail.com>
2024-12-18 12:30:59 +0100

    NAVBAR

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -4,17 +4,18 @@ const Nav = () => {
   return (
     <div className='fixed w-[100%] z-[1000] bg-gray-800'>
      <div className='flex items-center justify-between w-[80%] mx-auto h-[12vh]'>
-        <h1 className='flex-[0.6] cursor-pointer text-[25px] text-white font-bold'>
-            BRIGHTSTAR
-            <span className='text-yellow-300'>TECH</span>
-        </h1>
-        <div className='nav-link'>HOME</div>
-        <div className='nav-link'>SERVICES</div>
-        <div className='nav-link'>ABOUT</div>
-        <div className='nav-link'>PROJECT</div>
-        <div className='nav-link'>BLOG</div>
-        <div className='nav-link'>CONTACT</div>
-        </div>
+       <div className='font-mono text-white text-[18px]'>
+        <span className='text-[30px] md:text-yellow-400'>MR</span>
+        Coder
+       </div>
+       <ul className='md:flex hidden items-center space-x-10'>
+        <li><a className='nav_link' href='#'></a>Home</li>
+        <li><a className='nav_link' href='#'></a>About</li>
+        <li><a className='nav_link' href='#'></a>Service</li>
+        <li><a className='nav_link' href='#'></a>Blog</li>
+        <li><a className='nav_link' href='#'></a>Contact</li>
+       </ul>
+              </div>
         </div>
   )
 }
