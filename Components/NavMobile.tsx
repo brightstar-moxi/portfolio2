eee5d3594a83c9d5a38cfe9c0be8572667bcb008
unknown <brightstarmoxiz2@gmail.com>
2024-12-18 17:10:00 +0100

    MOBILE-NAVBAR

--- Components/NavMobile.tsx
+++ Components/NavMobile.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 const NavMobile = () => {
     return (
+        <div>
         <div className='fixed top-0 transform transition-all duration-500 z-[10000] left-0 right-0 bottom-0 bg-black opacity-70 w-[100vw] h-[100vh]'>
-            <ul className='text-white fixed flex items-center flex-col h-[100%] transform transition-all duration-300 delay-300 w-[60%] bg-red-600 space-y-14 z-[10000]'>
-                <li><a href="">Home</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Services</a></li>
-                <li><a href="">Blog</a></li>
-                <li><a href="">Contact</a></li>
+            </div>
+            <ul className='text-white fixed flex items-center flex-col justify-center h-[100%] transform transition-all duration-300 delay-300 w-[60%] bg-blue-900 space-y-14 z-[10000]'>
+                <li className='nav_link  text-[25px] sm:text-[30px]'><a href="#">Home</a></li>
+                <li className='nav_link  text-[25px] sm:text-[30px]'><a href="#">About</a></li>
+                <li className='nav_link  text-[25px] sm:text-[30px]'><a href="#">Services</a></li>
+                <li className='nav_link  text-[25px] sm:text-[30px]'><a href="#">Blog</a></li>
+                <li className='nav_link  text-[25px] sm:text-[30px]'><a href="#">Contact</a></li>
             </ul>
         </div>
     )
