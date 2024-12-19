8a40d96815d2a1d74ef065952a82f2546e646f1a
unknown <brightstarmoxiz2@gmail.com>
2024-12-19 18:16:30 +0100

    Hero Section

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='w-[100vw] pt-[4vh] md:pt-[12vh] h-[85vh] md:h-[100vh] custom-bg'>Hero</div>
+    <div className='w-[100vw] pt-[4vh] md:pt-[12vh] h-[85vh] md:h-[100vh] custom-bg'>
+        <div className='flex justify-center flex-col w-[80%] h-[100%]'></div>
+    </div>
   )
 }
 
