a68d29d6cb896d25dbd1c49ed99c7fbc5f90ccf4
unknown <brightstarmoxiz2@gmail.com>
2024-12-20 13:29:52 +0100

    Hero Section button

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -23,7 +23,7 @@ const Hero = () => {
                         <div className='mt-[2rem] flex items-center space-x-6'>
                             <button className='relation flex h-[50px] w-40 items-center justify-center font-semibold
                              overflow-hidden bg-red-500 text-white shadow-2xl transition-all before:absolute before:h-0 
-                             before:w-0 before:rounded-full before:bg-blue-600  before:duration-500 before:ease-out hover:before:h-56 hover:before:w-56'>
+                             before:w-0 before:rounded-full before:bg-blue-600 before:duration-500 before:ease-out hover:before:h-56 hover:before:w-56'>
                                 <span className='relative z-10'>Hire Me</span>
                              </button>
                         </div>
