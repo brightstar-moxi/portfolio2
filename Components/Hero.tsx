79c268034079e60c4a34bdeb5d5eb085f1e9adac
unknown <brightstarmoxiz2@gmail.com>
2024-12-20 13:57:56 +0100

    Hero Section button

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -21,11 +21,18 @@ const Hero = () => {
                             magni. Eligendi, harum unde?
                         </p>
                         <div className='mt-[2rem] flex items-center space-x-6'>
-                            <button className='relation flex h-[50px] w-40 items-center justify-center font-semibold
-                             overflow-hidden bg-red-500 text-white shadow-2xl transition-all before:absolute before:h-0 
-                             before:w-0 before:rounded-full before:bg-blue-600 before:duration-500 before:ease-out hover:before:h-56 hover:before:w-56'>
+                            <button className='relative flex h-[50px] w-40 items-center justify-center font-semibold
+                              overflow-hidden bg-red-500 text-white shadow-2xl transition-all before:absolute before:h-0 
+                             before:w-0 before:rounded-full before:bg-blue-600 before:duration-500 before:ease-out hover:before:h-56 
+                             hover:before:w-56'>
                                 <span className='relative z-10'>Hire Me</span>
                              </button>
+                             <button className='before:ease relative h-12 w-40 overflow-hidden bg-orange-600 text-white font-semibold 
+                             shadow-2xl before:absolute before:left-0 before:-ml-2 before:h-48 before:w-48 before:origin-top-right before:-translate-x-full 
+                             before:translate-y-12 before:-rotate-90 before:bg-green-700 before:transition-all before:duration-300 hover:text-white
+                              hover:shadow-black hover:before:-rotate-180'>
+                                <span className='relative z-10'>Download CV</span>
+                              </button>
                         </div>
                     </div>
                 </div>
