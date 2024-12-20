0921d0beb8d29ae2f3af2880b5cc413b8015d366
unknown <brightstarmoxiz2@gmail.com>
2024-12-20 12:54:22 +0100

    Hero Section

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import TypeWriteEffect from './TypeWriteEffect'
 
 const Hero = () => {
     return (
@@ -7,10 +8,18 @@ const Hero = () => {
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-[3rem] items-center'>
                     <div>
                         <h1 className='text-[#c4cfde] mb-[1.3rem]'>WELCOME TO BRIGHTSTAR WORLD</h1>
-                    <div>
-                        <h1 className='xl-text-[50px] lg:text-[40px] sm:text-[35px] text-[25px] font-semibold leading-[2.4rem] text-white'>
-                            Hi, I'm <span className='text-yellow-300'>Brightstar</span></h1>
-                    </div>
+                        <div>
+                            <h1 className='xl-text-[50px] lg:text-[40px] sm:text-[35px] text-[25px] font-semibold leading-[2.6rem] text-white'>
+                                Hi, I'm <span className='text-yellow-300'>Brightstar</span></h1>
+                               <TypeWriteEffect/> 
+                              
+                        </div>
+                        <p className=''>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem, 
+                            natus perferendis. Veritatis exercitationem voluptatum molestias in 
+                            nam quis aut neque animi ratione, cumque perferendis laudantium porro 
+                            magni. Eligendi, harum unde?
+                        </p>
                     </div>
                 </div>
             </div>
