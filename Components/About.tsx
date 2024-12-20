ecc438eb2ad703d1e58521312e8b0a19ab8a6f9e
unknown <brightstarmoxiz2@gmail.com>
2024-12-20 18:18:54 +0100

    About Section

--- Components/About.tsx
+++ Components/About.tsx
@@ -6,8 +6,8 @@ const About = () => {
         <div className='w-[80%] pt-[5rem] sm:pt-[2rem] md:pt-[16rem] 
         mx-auto grid items-center grid-cols-1 lg:grid-cols-2 gap-[2rem]'>
             <div>
-                <p>About Me</p>
-                <h1>Professional <span>Website</span>  for  your business</h1>
+                <p className='head_mini'>About Me</p>
+                <h1 className='heading_primary'>Professional <span className='text-yellow-400'>Website</span>  for  your business</h1>
             </div>
         </div>
     </div>
