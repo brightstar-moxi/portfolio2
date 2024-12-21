47979aeb5df5a70bce0c3b6d28232607d03e15f1
unknown <brightstarmoxiz2@gmail.com>
2024-12-21 22:27:33 +0100

    Services Section

--- Components/Services.tsx
+++ Components/Services.tsx
@@ -5,7 +5,10 @@ const Services = () => {
     <div className='pt-[3rem] pb-[3rem] bg-[#0b0c13]'>
         <div className='text-center'>
             <p className='heading_mini'>Popular Services</p>
-            <h1 className='heading_primary'>My Special <span>Services</span>for You</h1>
+            <h1 className='heading_primary'>My Special <span className='text-yellow-400'>Services</span> for You</h1>
+        </div>
+        <div className='pt-[5rem] w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[3rem] items-center'>
+            <div></div>
         </div>
     </div>
   )
