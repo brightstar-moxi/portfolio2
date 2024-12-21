f546ad07d862761cd7e4b3489df12907d8cdb335
unknown <brightstarmoxiz2@gmail.com>
2024-12-21 22:58:10 +0100

    ServicesCard Section

--- Components/ServicesCard.tsx
+++ Components/ServicesCard.tsx
@@ -1,14 +1,26 @@
+import { BugAntIcon, CodeBracketIcon, CommandLineIcon, CursorArrowRaysIcon, RocketLaunchIcon, ServerIcon } from '@heroicons/react/20/solid';
 import React from 'react';
 
 interface Props {
-    title:string;
-    num:string;
+    title: string; num: string;
+
 }
 
-const ServicesCard = ({num,title}:Props) => {
-  return (
-    <div>ServicesCard</div>
-  )
+const iconMapping = {
+    "01": CommandLineIcon,
+    "02": ServerIcon,
+    "03": CodeBracketIcon,
+    "04": CursorArrowRaysIcon,
+    "05": RocketLaunchIcon,
+    "06": BugAntIcon
+}
+
+const ServicesCard = ({ num, title }: Props) => {
+    const IconComponent =
+    (iconMapping as Record<string,React.ElementType>)[num] || CommandLineIcon;
+    return (
+        <div>ServicesCard</div>
+    )
 }
 
 export default ServicesCard
