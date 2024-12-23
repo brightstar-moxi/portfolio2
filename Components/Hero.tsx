b18fe8197f263ee04f6c4d4a0496683a1dcac2be
unknown <brightstarmoxiz2@gmail.com>
2024-12-23 15:02:33 +0100

    Skills Section

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -12,13 +12,13 @@ const Hero = () => {
                         <div>
                             <h1 className='xl-text-[50px] lg:text-[40px] sm:text-[35px] text-[25px] font-semibold leading-[2.6rem] text-white'>
                                 Hi, I'm <span className='text-yellow-300'>Brightstar</span></h1>
-                               <TypeWriteEffect/> 
-                              
+                            <TypeWriteEffect />
+
                         </div>
                         <p className='mt-[1.2rem] text-[15px] md:text-[17px] text-[#c4cfde]'>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem, 
-                            natus perferendis. Veritatis exercitationem voluptatum molestias in 
-                            nam quis aut neque animi ratione, cumque perferendis laudantium porro 
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem,
+                            natus perferendis. Veritatis exercitationem voluptatum molestias in
+                            nam quis aut neque animi ratione, cumque perferendis laudantium porro
                             magni. Eligendi, harum unde?
                         </p>
                         <div className='mt-[2rem] flex items-center space-x-6'>
@@ -27,22 +27,22 @@ const Hero = () => {
                              before:w-0 before:rounded-full before:bg-blue-600 before:duration-500 before:ease-out hover:before:h-56 
                              hover:before:w-56'>
                                 <span className='relative z-10'>Hire Me</span>
-                             </button>
-                             <button className='before:ease relative h-12 w-40 overflow-hidden bg-orange-600 text-white font-semibold 
+                            </button>
+                            <button className='before:ease relative h-12 w-40 overflow-hidden bg-orange-600 text-white font-semibold 
                              shadow-2xl before:absolute before:left-0 before:-ml-2 before:h-48 before:w-48 before:origin-top-right before:-translate-x-full 
                              before:translate-y-12 before:-rotate-90 before:bg-green-700 before:transition-all before:duration-300 hover:text-white
                               hover:shadow-black hover:before:-rotate-180'>
                                 <span className='relative z-10'>Download CV</span>
-                              </button>
+                            </button>
                         </div>
                     </div>
                     <div className='hidden md:block'>
-                        <Image 
-                        src="/image/aqw.png"
-                         alt='hero' 
-                         width={600} 
-                         height={600} 
-                         className='object-contain'/>
+                        <Image
+                            src="/image/aqw.png"
+                            alt='hero'
+                            width={600}
+                            height={600}
+                            className='object-contain' />
                     </div>
                 </div>
             </div>
--- Components/Skills.tsx
+++ Components/Skills.tsx
@@ -7,6 +7,16 @@ const Skills = () => {
             <div className='col-span-3'>
                 <p className='heading_mini'>My Skills</p>
                 <h1 className='heading_primary'>Let's Explore Popular <span className='text-yellow-400'>Skills</span> & Experience</h1>
+            <p className='text-[15px] mb-[1.5rem] text-white opacity-70 mt-[1.5rem]'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum nihil ut minus, alias magni minima ab beatae iusto? Pariatur 
+                facere consequatur sequi quidem eveniet nam porro ipsam ducimus sit amet?
+            </p>
+            <button className='relative flex h-[50px] w-40 items-center justify-center font-semibold
+                              overflow-hidden bg-orange-600 text-white shadow-2xl transition-all before:absolute before:h-0 
+                             before:w-0 before:rounded-full before:bg-blue-800 before:duration-500 before:ease-out hover:before:h-56 
+                             hover:before:w-56'>
+                                <span className='relative z-10'>Learn More</span>
+                            </button>
             </div>
         </div>
     </div>
