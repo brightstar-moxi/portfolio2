2a8c4fd1042bc7368430bae3285383ec16942fea
unknown <brightstarmoxiz2@gmail.com>
2024-12-23 14:14:47 +0100

    ServicesCard Section

--- Components/Services.tsx
+++ Components/Services.tsx
@@ -2,19 +2,35 @@ import React from 'react'
 import ServicesCard from './ServicesCard'
 
 const Services = () => {
-  return (
-    <div className='pt-[3rem] pb-[3rem] bg-[#0b0c13]'>
-        <div className='text-center'>
-            <p className='heading_mini'>Popular Services</p>
-            <h1 className='heading_primary'>My Special <span className='text-yellow-400'>Services</span> for You</h1>
-        </div>
-        <div className='pt-[5rem] w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[3rem] items-center'>
-            <div>
-                <ServicesCard title='React Website' num="01"/>
+    return (
+        <div className='pt-[3rem] pb-[3rem] bg-[#0b0c13]'>
+            <div className='text-center'>
+                <p className='heading_mini'>Popular Services</p>
+                <h1 className='heading_primary'>My Special <span className='text-yellow-400'>Services</span> for You</h1>
+            </div>
+            <div className='pt-[5rem] w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[3rem] items-center'>
+                <div>
+                    <ServicesCard title='React Website' num="01" />
+                </div>
+                <div>
+                    <ServicesCard title='Next JS Website' num="02" />
+                </div>
+                <div>
+                    <ServicesCard title='Fullstack Website' num="03" />
+                </div>
+                <div>
+                    <ServicesCard title=' Node JS Api' num="04" />
+                </div>
+                <div>
+                    <ServicesCard title='MERN Website' num="05" />
+                </div>
+                <div>
+                    <ServicesCard title='Bug Fixing' num="06" />
+                </div>
+
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Services
--- Components/ServicesCard.tsx
+++ Components/ServicesCard.tsx
@@ -21,7 +21,7 @@ const ServicesCard = ({ num, title }: Props) => {
     return (
         <div className='bg-black custom_service z-[100] relative transform rounded-2xl 
          text-center p-6 shadow-md'>
-            <IconComponent className="w-[5rem] relative z-[-1] mx-auto h-[rem]"/>
+            <IconComponent className="w-[5rem] relative z-[1] mx-auto h-[5rem] text-yellow-400"/>
             <h1 className='text-[25px] relative z-[1] text-white mt-[1rem]'>
                 {title}
                 </h1>
