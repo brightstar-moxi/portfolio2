30c780f8b6b673e10afd10d3d0177df7ec27c648
unknown <brightstarmoxiz2@gmail.com>
2024-12-23 14:46:07 +0100

    Skills Section

--- Components/Skills.tsx
+++ Components/Skills.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Skills = () => {
   return (
     <div className='pt-[5rem] pb-[3rem] bg-black'>
-        <div className=''></div>
+        <div className='grid w-[80%] mx-auto grid-cols-1 lg:grid-cols-7 gap-[2rem] items-center'>
+            <div className='col-span-3'>
+                <p className='heading_mini'>My Skills</p>
+                <h1 className='heading_primary'>Let's Explore Popular <span className='text-yellow-400'>Skills</span> & Experience</h1>
+            </div>
+        </div>
     </div>
   )
 }
