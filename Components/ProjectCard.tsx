a0543783e083a1eed36e9d66b8d355aeb21eae21
unknown <brightstarmoxiz2@gmail.com>
2024-12-25 20:04:19 +0100

    Project Card Section

--- Components/ProjectCard.tsx
+++ Components/ProjectCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react'
 
 interface Props{
@@ -12,7 +13,9 @@ interface Props{
 const ProjectCard = ({image, title, tech1, tech2, tech3, tech4}: Props) => {
   return (
     <div className='grid w-[80%] mx-auto pt-[5rem] grid-cols-1 lg:grid-cols-2 gap-10 items-center'>
-    <div className='p-4 rounded-xl'></div>
+    <div className='p-4 rounded-xl relative cursor-pointer hover:-rotate-6 transition-all duration-200 bg-gray-800 shadow-md'>
+        <Image src={`${image}`} alt={title} width={500} height={500} className='object-contain rounded-xl mx-auto shadow-md'/>
+    </div>
     </div>
   )
 }
