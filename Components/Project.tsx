29fca7db8be4373d180099e03d187ad998469ed3
unknown <brightstarmoxiz2@gmail.com>
2024-12-25 19:19:12 +0100

    Project Section

--- Components/Project.tsx
+++ Components/Project.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Project = () => {
     return (
-        <div>Project</div>
+        <div className='pt-[5rem] pb-[3rem] bg-gray-900'>
+            <div className='text-center'>
+                <p className='heading_mini'>Recent Works</p>
+                <h1 className='heading_primary'>My Best <span className='text-yellow-400'>Projects</span></h1>
+            </div>
+        </div>
     )
 }
 export default Project
