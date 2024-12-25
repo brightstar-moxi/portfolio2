defa09b607de3e5dd15bac3c80da8b511028e219
unknown <brightstarmoxiz2@gmail.com>
2024-12-25 19:52:42 +0100

    Project Card Section

--- Components/ProjectCard.tsx
+++ Components/ProjectCard.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const ProjectCard = () => {
+interface Props{
+    image: string;
+    title: string;
+    tech1: string;
+    tech2: string;
+    tech3: string;
+    tech4: string;
+}
+
+const ProjectCard = ({image, title, tech1, tech2, tech3, tech4}: Props) => {
   return (
-    <div>ProjectCard</div>
+    <div className='grid w-[80%] mx-auto pt-[5rem] grid-cols-1 lg:grid-cols-2 gap-10 items-center'>
+    <div className='p-4 rounded-xl'></div>
+    </div>
   )
 }
 
