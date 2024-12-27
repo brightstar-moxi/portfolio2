bbdfe56125fbcab940ac05f52ccb2e3ce68bc680
unknown <brightstarmoxiz2@gmail.com>
2024-12-27 02:32:04 +0100

    Project Price section

--- Components/PriceTable.tsx
+++ Components/PriceTable.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const PriceTable = () => {
   return (
-    <div className='pt-[5rem] pb-[3rem] bg-black'>PriceTable</div>
+    <div className='pt-[5rem] pb-[3rem] bg-black'>
+        <div className='text-center'>
+            <p className='heading_mini'>Popular Services</p>
+            <h1 className='heading_primary'>Best <span className='text-yellow-400'>Pricing</span> For My Project</h1>
+        </div>
+    </div>
   )
 }
 
