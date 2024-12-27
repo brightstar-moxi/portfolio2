5a7aea4ff00ce88849075054d101b7fb31cd91a0
unknown <brightstarmoxiz2@gmail.com>
2024-12-27 02:51:37 +0100

    Project Price section

--- Components/PriceTable.tsx
+++ Components/PriceTable.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import PriceCard from './PriceCard'
 
 const PriceTable = () => {
-  return (
-    <div className='pt-[5rem] pb-[3rem] bg-black'>
-        <div className='text-center'>
-            <p className='heading_mini'>Popular Services</p>
-            <h1 className='heading_primary'>Best <span className='text-yellow-400'>Pricing</span> For My Project</h1>
+    return (
+        <div className='pt-[5rem] pb-[3rem] bg-black'>
+            <div className='text-center'>
+                <p className='heading_mini'>Popular Services</p>
+                <h1 className='heading_primary'>Best <span className='text-yellow-400'>Pricing</span> For My Project</h1>
+            </div>
+            <div className='w-[80] mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 items-center pt-[4rem] md:pt-[7rem]'>
+                <PriceCard plan="Basic" Price="20$" save="25%" bg="bg-red-900" />
+            </div>
         </div>
-        <div className='w-[80] mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 items-center pt-[4rem] md:pt-[7rem]'></div>
-    </div>
-  )
+    )
 }
 
 export default PriceTable
