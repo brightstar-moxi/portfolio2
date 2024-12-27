3cc78b8da8387e94497d29faf4a1a3fcfe54bf9f
unknown <brightstarmoxiz2@gmail.com>
2024-12-27 04:02:59 +0100

    Project Price section

--- Components/PriceTable.tsx
+++ Components/PriceTable.tsx
@@ -10,6 +10,8 @@ const PriceTable = () => {
             </div>
             <div className='w-[80%] mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 items-center pt-[4rem] md:pt-[7rem]'>
                 <PriceCard plan="Basic" price="20$" save="25%" bg="bg-red-900" />
+                <PriceCard plan="Popular" price="40$" save="35%" bg="bg-green-900" />
+                <PriceCard plan="Premium" price="60$" save="45%" bg="bg-blue-900" />
             </div>
         </div>
     )
