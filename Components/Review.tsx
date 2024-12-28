e813468ef0c394cac0910e266ab0a136ef56fa65
unknown <brightstarmoxiz2@gmail.com>
2024-12-28 02:53:07 +0100

    Review section

--- Components/Review.tsx
+++ Components/Review.tsx
@@ -7,6 +7,7 @@ const Review = () => {
             <div className='heading_mini'>Client Reviews</div>
             <h1 className='heading_primary'>I've <span className='text-yellow-400'>40+</span> Clients Feedback</h1>
         </div>
+        <div className='w-[80%] pt-[3rem] md:pt-[5rem] mx-auto'></div>
     </div>
   )
 }
