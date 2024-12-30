414b9818d6d43b0d47fc081f406a1a72cef87964
unknown <brightstarmoxiz2@gmail.com>
2024-12-30 20:11:20 +0100

    contact section

--- Components/Contact.tsx
+++ Components/Contact.tsx
@@ -7,6 +7,11 @@ const Contact = () => {
             <div>
                 <p className='heading_mini'>Get in Touch</p>
                 <h1 className='heading_primary'>Let's make your <span className='text-yellow-400'>Brand</span> brilliant</h1>
+            <p className='text-[15px] text-white mt-[1rem] opacity-75'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque cumque eius, tenetur excepturi accusamus est, nulla 
+                maxime earum ad iure error fugiat expedita sit? Minus porro veniam numquam fuga doloribus.
+                </p>
+                <h1 className=''>+1 234 56 7890</h1>
             </div>
         </div>
     </div>
