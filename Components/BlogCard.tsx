a183a50795dcf2ced17645cdc93875e747cb02c6
unknown <brightstarmoxiz2@gmail.com>
2024-12-30 19:00:08 +0100

    blog card section

--- Components/BlogCard.tsx
+++ Components/BlogCard.tsx
@@ -24,8 +24,10 @@ const BlogCard = ({image, title, comment, date}:Props) => {
         </div>
         <div className='p-4'>
             <div className='mt-[1rem] mb-[1rem] px-3 py-1 bg-gray-800 w-fit flex items-center space-x-2 rounded-lg'>
-                <CalendarIcon/>
+                <CalendarIcon className='w-[1rem] h-[1rem] text-yellow-400'/>
+                <p className='text-white text-[14px] opacity-85'>{date}</p>
             </div>
+            <h1 className='text-[20px] opacity-85 hover:text-yellow-400 transition-all duration-200 cursor-pointer text-white font-semibold'>{title}</h1>
         </div>
     </div>
   )
