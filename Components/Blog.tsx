773e1e0248a939c3371fedee741caad28c5e42a5
unknown <brightstarmoxiz2@gmail.com>
2024-12-30 18:25:03 +0100

    blog section

--- Components/Blog.tsx
+++ Components/Blog.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import BlogCard from './BlogCard'
 
 const Blog = () => {
   return (
@@ -6,6 +7,9 @@ const Blog = () => {
         <div className='text-center'>
             <p className='heading_mini'>My Blog</p>
             <h1 className='heading_primary'>My Latest <span className='text-yellow-400'>Blog</span> and News</h1>
+        </div>
+        <div className='w-[80%] mx-auto pt-[3rem] md:pt-[5rem] grid grid-cols-2 lg:grid-cols-3 items-center gap-[2rem]'>
+            <div><BlogCard/></div>
         </div>
     </div>
   )
