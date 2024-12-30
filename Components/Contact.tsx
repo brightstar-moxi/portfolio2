3e0b5efafb4400da0050377a3799632664398e7f
unknown <brightstarmoxiz2@gmail.com>
2024-12-30 20:01:28 +0100

    contact section

--- Components/Contact.tsx
+++ Components/Contact.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className='pt-[5rem] pb-[3rem] bg-gray-900'>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-[2rem] items-center w-[80%] mx-auto'></div>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-[2rem] items-center w-[80%] mx-auto'>
+            <div>
+                <p className='heading_mini'>Get in Touch</p>
+                <h1 className='heading_primary'>Let's make your <span className='text-yellow-400'>Brand</span> brilliant</h1>
+            </div>
+        </div>
     </div>
   )
 }
