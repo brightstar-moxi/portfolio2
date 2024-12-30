fc17e68cbe0f964fb778a795be8f264a09f79e36
unknown <brightstarmoxiz2@gmail.com>
2024-12-30 19:52:29 +0100

    contact section

--- Components/Contact.tsx
+++ Components/Contact.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='pt-[5rem] pb-[3rem] bg-gray-900'>Contact</div>
+    <div className='pt-[5rem] pb-[3rem] bg-gray-900'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-[2rem] items-center w-[80%] mx-auto'></div>
+    </div>
   )
 }
 
