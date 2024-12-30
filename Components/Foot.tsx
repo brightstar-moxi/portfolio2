06ae2c85360f9739d61b4f91d58c5b7ed131127c
unknown <brightstarmoxiz2@gmail.com>
2024-12-30 21:29:10 +0100

    contact section

--- Components/Foot.tsx
+++ Components/Foot.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Foot = () => {
   return (
     <div className='pt-[3rem] pb-[3rem] bg-black'>
-        <div></div>
+        <div className='w-[80%] mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[2rem] border-b-[1.4rem] pb-5 border-gray-600 border-opacity-40'></div>
     </div>
   )
 }
