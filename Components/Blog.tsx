89cfee276ee37a63216e6fb436a4dec635836c36
unknown <brightstarmoxiz2@gmail.com>
2024-12-30 11:46:45 +0100

    blog section

--- Components/Blog.tsx
+++ Components/Blog.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='pt-[5rem] pb-[5rem] bg-black'>
+        <div className='text-center'>
+            <p className='heading_mini'>My Blog</p>
+            <h1 className='heading_primary'>My Latest <span className='text-yellow-400'>Blog</span> and News</h1>
+        </div>
+    </div>
   )
 }
 
