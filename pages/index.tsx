d05fda1a4596be2ca80cfe129840066fa72dc571
unknown <brightstarmoxiz2@gmail.com>
2024-12-31 16:18:01 +0100

    Footer section

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,13 @@ import PriceTable from '@/Components/PriceTable';
 import Project from '@/Components/Project';
 import Services from '@/Components/Services';
 import Skills from '@/Components/Skills';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Review from '../Components/Review';
 import Blog from '@/Components/Blog';
 import Contact from '@/Components/Contact';
 import Foot from '@/Components/Foot';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 const HomePage = () => {
@@ -18,6 +20,11 @@ const HomePage = () => {
   const [showNav, setShowNav] = useState(false);
   const showNavHandler = () => setShowNav(true);
   const closeNavHandler = () => setShowNav(false);
+
+  // animation
+  useEffect(()=>{
+
+  },[])
 
   return (
     <div className='overflow-hidden '>
