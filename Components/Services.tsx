d8c891306d2794260f108a31698a9adad1b0f1df
unknown <brightstarmoxiz2@gmail.com>
2025-01-02 05:21:31 +0100

    animation

--- Components/Services.tsx
+++ Components/Services.tsx
@@ -9,22 +9,22 @@ const Services = () => {
                 <h1 className='heading_primary'>My Special <span className='text-yellow-400'>Services</span> for You</h1>
             </div>
             <div className='pt-[5rem] w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[3rem] items-center'>
-                <div>
+                <div data-aos='fade-up' data-aos-anchor-placement='top-center' >
                     <ServicesCard title='React Website' num="01" page=' Lorem ipsum dolor sit amet consectetur adipisicing elit. 1' />
                 </div>
-                <div>
+                <div data-aos='fade-up' data-aos-anchor-placement='top-center' data-aos-delay="200">
                     <ServicesCard title='Next JS Website' num="02" page=' Lorem ipsum dolor sit amet consectetur adipisicing elit. 2' />
                 </div>
-                <div>
+                <div data-aos='zoom-in' data-aos-anchor-placement='top-center' data-aos-delay="400">
                     <ServicesCard title='Fullstack Website' num="03" page=' Lorem ipsum dolor sit amet consectetur adipisicing elit. 3' />
                 </div>
-                <div>
+                <div data-aos='zoom-in' data-aos-anchor-placement='top-center' data-aos-delay="600">
                     <ServicesCard title=' Node JS Api' num="04" page=' Lorem ipsum dolor sit amet consectetur adipisicing elit. 4' />
                 </div>
-                <div>
+                <div data-aos='zoom-in' data-aos-anchor-placement='top-center' data-aos-delay="800">
                     <ServicesCard title='MERN Website' num="05" page=' Lorem ipsum dolor sit amet consectetur adipisicing elit. 5' />
                 </div>
-                <div>
+                <div data-aos='zoom-in' data-aos-anchor-placement='top-center' data-aos-delay="1000">
                     <ServicesCard title='Bug Fixing' num="06" page=' Lorem ipsum dolor sit amet consectetur adipisicing elit. 6' />
                 </div>
 
