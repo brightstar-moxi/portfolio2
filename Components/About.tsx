220eae29b410b50afa8c01470e582dcdd7cfbbf2
unknown <brightstarmoxiz2@gmail.com>
2025-01-02 05:16:21 +0100

    animation

--- Components/About.tsx
+++ Components/About.tsx
@@ -13,19 +13,19 @@ const About = () => {
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, dicta reprehenderit unde praesentium perferendis commodi? Maiores necessitatibus accusantium molestias ea atque. Amet debitis similique ex cupiditate, magnam quisquam tempore delectus!
                     </p>
                     <div className='mt-[2rem] space-y-3'>
-                        <div data-aos='zoom-in' data-aos-anchor-placement='top-center' className='flex items-center space-x-4'>
+                        <div className='flex items-center space-x-4'>
                             <CheckIcon className='w-[2rem] h-[2rem] text-yellow-600' />
                             <p className='text-[18px] text-white'>Frontend Development</p>
                         </div>
-                        <div data-aos='zoom-in' data-aos-anchor-placement='top-center' data-aos-delay="200" className='flex items-center space-x-4'>
+                        <div className='flex items-center space-x-4'>
                             <CheckIcon className='w-[2rem] h-[2rem] text-yellow-600' />
                             <p className='text-[18px] text-white'>Backend Development</p>
                         </div>
-                        <div data-aos='zoom-in' data-aos-anchor-placement='top-center' data-aos-delay="400" className='flex items-center space-x-4'>
+                        <div className='flex items-center space-x-4'>
                             <CheckIcon className='w-[2rem] h-[2rem] text-yellow-600' />
                             <p className='text-[18px] text-white'>Web Development</p>
                         </div>
-                        <div data-aos='zoom-in' data-aos-anchor-placement='top-center' data-aos-delay="600" className='flex items-center space-x-4'>
+                        <div className='flex items-center space-x-4'>
                             <CheckIcon className='w-[2rem] h-[2rem] text-yellow-600' />
                             <p className='text-[18px] text-white'>MERN Development</p>
                         </div>
@@ -33,19 +33,19 @@ const About = () => {
                 </div>
                 <div className='lg:ml-auto'>
                     <div className='grid grid-cols-1 sm:grid-cols-2 items-center gap-[2rem]'>
-                        <div className='p-6 text-center bg-yellow-500'>
-                            <p className='text-[50px] text-black font-bold'>3</p>
+                        <div data-aos='zoom-in' data-aos-anchor-placement='top-center'  className='p-6 text-center bg-yellow-500'>
+                            <p className='text-[50px] text-white font-bold'>3</p>
                             <p className='text-[20px] text-black font-[600]'>Years experience</p>
                         </div>
-                        <div className='p-6 text-center bg-yellow-500'>
+                        <div data-aos='zoom-in' data-aos-anchor-placement='top-center' data-aos-delay="200" className='p-6 text-center bg-yellow-500'>
                             <p className='text-[50px] text-black font-bold'>200+</p>
                             <p className='text-[20px] text-black font-[600]'>Happy Client</p>
                         </div>
-                        <div className='p-6 text-center bg-yellow-500'>
+                        <div data-aos='zoom-in' data-aos-anchor-placement='top-center' data-aos-delay="400" className='p-6 text-center bg-yellow-500'>
                             <p className='text-[50px] text-black font-bold'>300+</p>
                             <p className='text-[20px] text-black font-[600]'>Project Done</p>
                         </div>
-                        <div className='p-6 text-center bg-yellow-500'>
+                        <div data-aos='zoom-in' data-aos-anchor-placement='top-center' data-aos-delay="600" className='p-6 text-center bg-yellow-500'>
                             <p className='text-[50px] text-black font-bold'>3+</p>
                             <p className='text-[20px] text-black font-[600]'>Award</p>
                         </div>
